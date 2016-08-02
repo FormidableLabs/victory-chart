454011f572672cc4e84ccdb6f2aeed6348101ac9
ebrillhart <emma.brillhart@gmail.com>
2016-08-02 10:09:41 -0700

    fixes a problem that was causing data labels to not appear for candlestick charts

--- demo/components/victory-candlestick-demo.js
+++ demo/components/victory-candlestick-demo.js
@@ -31,10 +31,10 @@ const style = {
 
 const data = [
   {x: new Date(2016, 6, 1), open: 9, close: 30, high: 56, low: 7},
-  {x: new Date(2016, 6, 2), open: 80, close: 40, high: 120, low: 10},
+  {x: new Date(2016, 6, 2), open: 80, close: 40, high: 120, low: 10, label: "label"},
   {x: new Date(2016, 6, 3), open: 50, close: 80, high: 90, low: 20},
   {x: new Date(2016, 6, 4), open: 70, close: 22, high: 70, low: 5},
-  {x: new Date(2016, 6, 5), open: 20, close: 35, high: 50, low: 10},
+  {x: new Date(2016, 6, 5), open: 20, close: 35, high: 50, low: 10, label: "hi"},
   {x: new Date(2016, 6, 6), open: 35, close: 30, high: 40, low: 3},
   {x: new Date(2016, 6, 7), open: 30, close: 90, high: 95, low: 30},
   {x: new Date(2016, 6, 8), open: 80, close: 81, high: 83, low: 75}
@@ -110,79 +110,7 @@ export default class App extends React.Component {
         />
         </svg>
 
-        <VictoryCandlestick
-          style={{parent: style.parent}}
-          data={data}
-          theme={VictoryTheme.material}
-          size={8}
-          events={[{
-            target: "labels",
-            eventHandlers: {
-              onClick: () => {
-                return [
-                  {
-                    mutation: (props) => {
-                      return {
-                        style: merge({}, props.style.labels, {fill: "orange"})
-                      };
-                    }
-                  }
-                ];
-              }
-            }
-          },
-          {
-            target: "data",
-            eventHandlers: {
-              onClick: () => {
-                return [
-                  {
-                    mutation: (props) => {
-                      return {
-                        style: merge({}, props.style, {fill: "blue"})
-                      };
-                    }
-                  }
-                ];
-              }
-            }
-          }]}
-        />
-
-        <VictoryChart
-          scale={{x: "time"}}
-          style={{
-            parent: style.parent
-          }}
-          domainPadding={{x: [20, 50]}}
-        >
-          <VictoryCandlestick
-            candleColors={{positive: "#8BC34A", negative: "#C62828"}}
-            data={data}
-            style={{data: {stroke: "none"}}}
-            size={8}
-          />
-        </VictoryChart>
-
-        <VictoryCandlestick
-          animate={{duration: 2000}}
-          data={this.state.data}
-          style={{
-            data: { width: 50, stroke: "transparent" },
-            parent: style.parent
-          }}
-        />
-
-        <VictoryCandlestick
-          size={1}
-        />
-
-        <VictoryChart>
-          <VictoryCandlestick
-            data={[]}
-          />
-        </VictoryChart>
-
+       
       </div>
     );
   }
--- src/components/victory-candlestick/victory-candlestick.js
+++ src/components/victory-candlestick/victory-candlestick.js
@@ -38,7 +38,7 @@ const defaultData = [
   {x: new Date(2016, 6, 1), open: 5, close: 10, high: 15, low: 0},
   {x: new Date(2016, 6, 2), open: 10, close: 15, high: 20, low: 5},
   {x: new Date(2016, 6, 3), open: 15, close: 20, high: 25, low: 10},
-  {x: new Date(2016, 6, 4), open: 20, close: 25, high: 30, low: 15},
+  {x: new Date(2016, 6, 4), open: 20, close: 25, high: 30, low: 15, label: "helloooo"},
   {x: new Date(2016, 6, 5), open: 25, close: 30, high: 35, low: 20},
   {x: new Date(2016, 6, 6), open: 30, close: 35, high: 40, low: 25},
   {x: new Date(2016, 6, 7), open: 35, close: 40, high: 45, low: 30},
@@ -496,8 +496,10 @@ export default class VictoryCandlestick extends React.Component {
         components[index] = React.cloneElement(
           groupComponent, {key: `candle-group-${key}`}, candleComponent, candleLabel
         );
+      } else {
+        components[index] = candleComponent;
       }
-      components[index] = candleComponent;
+      
     }
     return components;
   }
@@ -542,7 +544,7 @@ export default class VictoryCandlestick extends React.Component {
       // make sense to tween. In the future, allow customization of animated
       // prop whitelist/blacklist?
       const whitelist = [
-        "data", "domain", "height", "maxBubbleSize", "padding", "samples", "size",
+        "data", "domain", "height", "padding", "samples", "size",
         "style", "width", "x", "y"
       ];
       return (
