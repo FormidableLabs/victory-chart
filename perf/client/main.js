3bd66d3a152cc51dd825692206b27dff12399e38
Brian Beck <exogen@gmail.com>
2016-09-05 11:48:41 -0700

    Expose Perf in entry module

--- perf/client/main.js
+++ perf/client/main.js
@@ -1,3 +1,7 @@
+const Perf = require("react-addons-perf");
+
+window.Perf = Perf;
+
 // Use webpack to infer and `require` tests automatically.
 const benchesReq = require.context(".", true, /\.bench.js?$/);
 benchesReq.keys().map(benchesReq);
